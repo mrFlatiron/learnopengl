9e5b6f4c29fb80f976928b3aa91d637f3f87b026
mrflatiron <mr.flatiron@mail.ru>
2018-10-05 23:22:27 +0300

    hack worked!

--- config/app_config.h
+++ config/app_config.h
@@ -13,9 +13,10 @@ namespace app_config
 constexpr const char *SRC_PATH = "/home/yozhek/learnopengl/src/";
 constexpr const char *RESOURCES_PATH = "/home/yozhek/learnopengl/resources/";
 #else
-static_assert (false, "Define your own paths here and move this assert below them. "
-               "Guard it with your new macro. Write a following line to config_compilation.hack"
-               "QMAKE_CXXFLAGS += -DYOUR_GUARD_MACRO");
+static_assert (false, "\n"
+                      "Define your own paths here and move this assert below them.\n"
+                      "Guard it with your new macro. Write a following line to config_compilation.hack\n"
+                      "QMAKE_CXXFLAGS += -DYOUR_GUARD_MACRO\n");
 constexpr const char *SRC_PATH = "";
 constexpr const char *RESOURCES_PATH = "";
 #endif
